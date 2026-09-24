b790c60dd21d54213e3d94eb9cbd21eb81198d5d
Van Vu <vanvu19102003@gmail.com>
2026-09-24 16:04:57 +0700

    Buoi02 Bai02 In thong tin ca nhan

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,14 @@
+// Nguyen Van Vu - 25810053
+
+fun inThongTin(){
+    var hoTen: String = "Nguyễn Văn Vũ";
+    var namSinh: Int = 2003;
+
+    val tinhTuoi = 2026 - namSinh
+
+    println("HỌ VÀ TÊN:  $hoTen \nTUỔI: $tinhTuoi")
+}
+
+fun main(){
+    inThongTin()
+}
